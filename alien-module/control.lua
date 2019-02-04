adb43a7d4a305a2e3801bdeebff6f985d85e7a28
renoth <johannes.renoth@gmx.de>
2019-02-04 21:12:05 +0100

    0.5.2: - Ingredients are no longer lost when upgrading an assembly machine currently crafting an alien hyper module (Thanks to unhott for providing the fix)

--- alien-module/control.lua
+++ alien-module/control.lua
@@ -111,11 +111,14 @@ function update_recipes(assemblers, force)
         if entity.get_recipe() ~= nil then --if the assembler has a set recipe
             if string.find(entity.get_recipe().name, "^alien%-hyper%-module") then --and its one of ours
                 local plates_to_refund = 0
-                if entity.is_crafting() then
-                    plates_to_refund = (global.currentmodulelevel-1) * 20 
+
+                if entity.is_crafting() then -- refund ingredients if hyper modules are being crafted
+                    plates_to_refund = (global.currentmodulelevel - 1) * 20 -- cost of currently crafting recipe
+                    plates_to_refund = math.max(plates_to_refund, 0) -- dont add negative amount
+                    entity.get_inventory(defines.inventory.assembling_machine_input).insert { name = "alien-plate", count = plates_to_refund }
                 end
+
                 entity.set_recipe(force.recipes["alien-hyper-module-" .. global.currentmodulelevel]) --set it to the updated recipe
-                entity.get_inventory(defines.inventory.assembling_machine_input).insert{name = "alien-plate", count = plates_to_refund}
             end
         end
     end

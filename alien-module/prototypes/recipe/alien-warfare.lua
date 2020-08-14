d0856f310db0b9bf001d5997ad1456a796a9c377
Stefan Simon <stefansimon76@googlemail.com>
2020-08-14 10:15:09 +0200

    - add alien steam engine - add alien ore magazine - change recipe alien magazine
    - add alien steam engine
    - add alien ore magazine
    - change recipe alien magazine

--- alien-module/prototypes/recipe/alien-warfare.lua
+++ alien-module/prototypes/recipe/alien-warfare.lua
@@ -1,14 +1,30 @@
 -- alien-magazine --
 data:extend({
-	{
-		type = "recipe",
-		name = "alien-magazine",
-		enabled = false,
-		energy_required = 10,
-		result = "alien-magazine",
-		result_count = 1,
-		ingredients = {
-			{ "alien-plate", 2 }, { "iron-plate", 6 }, { "copper-plate", 1 }
-		},
-	},
+    {
+        type = "recipe",
+        name = "alien-magazine",
+        enabled = false,
+        energy_required = 10,
+        result = "alien-magazine",
+        result_count = 1,
+        ingredients =
+        {
+            { "alien-plate", 2 }, { "iron-plate", 2 }, { "copper-plate", 1 },
+        },
+    },
+})
+
+data:extend({
+    {
+        type = "recipe",
+        name = "alien-ore-magazine",
+        enabled = false,
+        energy_required = 2,
+        result = "alien-ore-magazine",
+        result_count = 1,
+        ingredients =
+        {
+            { "artifact-ore", 2 },
+        },
+    },
 })
